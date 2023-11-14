c2fea1f84966297951e42e0f1408725618683733
yusuf1 <yusufsmik@gmail.com>
2023-11-14 23:06:00 +0800

    scoden comit

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     <article>
         <h1>
-            <a href="/posts/<?= $post->slug ?>">
+            
                 {{ $post->title }}
-            </a>
+            
         </h1>
         <p>{!! $post->body !!}</p>
     </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,8 +4,8 @@
     @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
+                <a href="/posts/{{ $post->id }}">
+                    {!! $post->title !!}
                 </a>
             </h1>
             <p>{{ $post->excerpt }}</p>
